7f5a298d83c25f20b4df888a16c8bdf82cf3c9c6
martin1906s <martin.simbana007@gamil.com>
2024-09-26 13:59:06 -0500

    Implementacion de funciones en testCalculadora

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -8,8 +8,17 @@ public class TestCalculadora {
 		double resultadoResta;
 		resultadoSuma=calcu.sumar(5, 3);
 		resultadoResta=calcu.restar(10, 3);
+		
+		double resultadoMultiplicar=calcu.multiplicar(10, 5);
+		double resultadoDividir=calcu.dividr(10, 2);
+		double resultadoPromedio=calcu.promediar(10, 8, 9);
+		
 		System.out.println("Resultado Suma: "+ resultadoSuma);
 		System.out.println("Resultado Resta: "+ resultadoResta);
+		System.out.println("Resultado Multiplicar: "+ resultadoMultiplicar);
+		System.out.println("Resultado Dividir: "+ resultadoDividir);
+		System.out.println("Resultado Promedio: "+ resultadoPromedio);
+		calcu.mostrarResultado();
 	}
 
 }
