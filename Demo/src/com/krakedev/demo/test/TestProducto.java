2d5c8c67e45a66aba97a9ca706f339e2a18bc1cf
martin1906s <martin.simbana007@gamil.com>
2024-09-28 21:59:58 -0500

    Reto 14

---
+++ Demo/src/com/krakedev/demo/test/TestProducto.java
@@ -0,0 +1,17 @@
+package com.krakedev.demo.test;
+
+import com.krakedev.demo.Producto;
+
+public class TestProducto {
+
+	public static void main(String[] args) {
+		Producto producto1= new Producto(20245,"Leche");
+		producto1.setDescripcion("Vitta");
+		producto1.setPeso(100);
+		System.out.println("COdigo: "+producto1.getCodigo());
+		System.out.println("Nombre: "+producto1.getNombre());
+		System.out.println("Descripcion: "+producto1.getDescripcion());
+		System.out.println("Peso: "+producto1.getPeso());
+	}
+
+}
